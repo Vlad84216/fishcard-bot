53b866299adad66673e765a845209266d6d3ef2a
Vlad84216 <navexpert_solution@mail.ru>
2025-07-24 13:24:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -49,11 +49,12 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     except Exception as e:
         await update.message.reply_text(f"Ошибка анализа фото: {e}")
 
-async def main():
+def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.PHOTO, handle_photo))
-    await app.run_polling()
+    app.run_polling()
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
