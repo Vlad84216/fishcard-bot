364796b9facd4fa63c7664278b10ca19c8b3c691
Vlad84216 <navexpert_solution@mail.ru>
2025-07-24 07:31:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # фиктивный коммит для перезапуска деплоя
+import asyncio
+
+async def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.PHOTO, handle_photo))
+    await app.run_polling()
+
+if __name__ == '__main__':
+    asyncio.run(main())
